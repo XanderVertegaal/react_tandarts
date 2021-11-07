050874d6b13d6ff499233b8866ca5912516b7d5a
Xander Vertegaal <xander.vertegaal@xs4all.nl>
2021-11-07 16:20:41 +0100

    Patients and assistants can be sick

--- src/AppointmentInMonth.js
+++ src/AppointmentInMonth.js
@@ -2,14 +2,26 @@ import React from "react";
 
 const format_time = time => (time < 10 ? `0${time}:00u` : `${time}:00u`);
 
-const AppointmentInMonth = ({ time, patient }, isDentistSick = false) => {
-
-  const sickStyle = (isDentistSick === true ? {backgroundColor: 'blue'} : {})
+const AppointmentInMonth = props => {
+  let sickStyle = {}
+  if (props.cancelled === true) {
+    return (
+      <div className="appointment">
+        <span className="time">{format_time(props.appointment.time)}</span>
+        <span className="patient">CANCELLED!</span>
+      </div>
+    );
+    } else if (props.sickPeople.dentist.includes(props.appointment.dentist.id)) {
+    console.log('Hit!', props.appointment, props.sickPeople)
+    sickStyle = {backgroundColor: 'red'}
+    } else if (props.sickPeople.assistant && props.sickPeople.assistant.includes(props.appointment.assistant.id) === true) {
+      sickStyle = {backgroundColor: 'yellow'}
+    }
 
   return (
     <div className="appointment" style={sickStyle}>
-      <span className="time">{format_time(time)}</span>
-      <span className="patient">{patient}</span>
+      <span className="time">{format_time(props.appointment.time)}</span>
+      <span className="patient">{`${props.appointment.patient.firstName} ${props.appointment.patient.lastName}`}</span>
     </div>
   );
 }
--- src/DayInMonth.js
+++ src/DayInMonth.js
@@ -5,8 +5,8 @@ const DayInMonth = props => {
 
   const appointmentsJSX = props.appointments.map((appointment, index) => {
       if (props.sickPeople.patient.includes(appointment.patient.id) === false) {
-          return ( <AppointmentInMonth time={appointment.time} patient={`${appointment.patient.firstName} ${appointment.patient.lastName}`} key={index} />)} else {
-            return <AppointmentInMonth time={appointment.time} patient={`CANCELLED!`} key={index} />
+          return ( <AppointmentInMonth appointment={appointment} key={index} sickPeople={props.sickPeople} cancelled={false}/>)} else {
+            return <AppointmentInMonth appointment={appointment} key={index} sickPeople={props.sickPeople} cancelled={true}/>
           }
       });
 
